17516540a2d1bdc5af4326b931a9a07a696f6cd9
AnnyaB <anabordin2008@gmail.com>
2024-04-21 13:17:56 -0300

    finalizacao exercicio

--- CalcularHipotenusa/Program.cs
+++ CalcularHipotenusa/Program.cs
@@ -1,1 +1,30 @@
 ﻿//4. faça um programa que leia 2 valores de cateto e calcule a hipotenusa;
+
+int res = 0;
+
+Console.WriteLine("-Calcule a hipotenusa-\n");
+do
+{
+    double cat1 = 0, cat2 = 0, hip = 0;
+    
+    while (cat1 <= 0)
+    {
+        Console.WriteLine("Digite o tamanho do primeiro cateto:");
+        cat1 = double.Parse(Console.ReadLine());
+    }
+
+    while (cat2 <= 0)
+    {
+        Console.WriteLine("Digite o tamanho do segundo cateto:");
+        cat2 = double.Parse(Console.ReadLine());
+    }
+
+    hip = (cat1 * cat1) + (cat2 * cat2);
+    hip = Math.Pow(cat1, 2) + Math.Pow(cat2, 2);
+    hip = Math.Sqrt(hip);
+
+    Console.WriteLine($"\nA hipotenusa é:\n{hip}\n");
+    Console.WriteLine("Deseja continuar?\nDigite 0, se não, digite outro número:");
+    res = int.Parse(Console.ReadLine());
+
+} while (res == 0);
